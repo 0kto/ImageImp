63370b72de16e4a15d4877065befac853f523ddb
mjk <warlock-mjk@0kto.net>
2020-02-26 20:58:20 +0100

    make sure ExifTools has executed the command

--- iimg.py
+++ iimg.py
@@ -60,10 +60,11 @@ def extract_embedded_jpg(files):
 		if not os.path.isfile(f"{basename}.cr2"):
 			print(f"no .cr2 file for '{basename}' available.")
 		elif os.path.isfile(f"{basename}.jpg"):
-			print(f".jpg file already exists for '{basename}'")
+			print(f"pre-existing '{basename}'.jpg")
 		else:
 			with ExifTool() as e:
 				metadata = e.extract_embedded_jpg(f"{basename}.cr2")
+			print(f"extracted {basename}.jpg")
 
 if __name__ == "__main__":
 	main()
