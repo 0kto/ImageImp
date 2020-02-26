6e42950d36e9a23f622be5cdef48e65b1693d428
mjk <warlock-mjk@0kto.net>
2020-02-26 21:29:08 +0100

    add multiprocessing

--- iimg/iimg.py
+++ iimg/iimg.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 import sys, getopt, os
+from multiprocessing import Pool
+
+# import definitions and classes
 import class_ExifTool
 
 def main():
@@ -14,7 +17,8 @@ def main():
 			usage()
 			sys.exit()
 		elif o in ("-e", "--extract"):
-			extract_embedded_jpg(args)
+			with Pool(5) as p:
+				p.map(extract_embedded_jpg, args)
 
 def usage():
 	print("iimg options <filelist>")
@@ -22,17 +26,18 @@ def usage():
 	print("available options")
 	print("-e, --extract     extract embedded .jpg from .cr2")
 
-def extract_embedded_jpg(files):
-	for file in files:
-		basename, ext = os.path.splitext(file)
-		if not os.path.isfile(f"{basename}.cr2"):
-			print(f"no .cr2 file for '{basename}' available.")
-		elif os.path.isfile(f"{basename}.jpg"):
-			print(f"pre-existing '{basename}'.jpg")
-		else:
-			with ExifTool() as e:
-				metadata = e.extract_embedded_jpg(f"{basename}.cr2")
-			print(f"extracted {basename}.jpg")
+def extract_embedded_jpg(file):
+	basename, ext = os.path.splitext(file)
+	if not os.path.isfile(f"{basename}.cr2"):
+		print(f"ignoring '{basename}'")
+
+	elif os.path.isfile(f"{basename}.jpg"):
+		print(f"pre-existing '{basename}'.jpg")
+
+	else:
+		with ExifTool() as e:
+			metadata = e.extract_embedded_jpg(f"{basename}.cr2")
+		print(f"extracted {basename}.jpg")
 
 if __name__ == "__main__":
 	main()
